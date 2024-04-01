2f98cc8e491e26547fa14330d04e8920a3376d65
ibraheem Zeer <211400@ppu.edu.ps>
2024-04-01 03:42:43 +0300

    display category , put server is down...

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
 
 export default function Categories() {
-  return <div>Categories</div>;
+  const [products, setProducts] = useState(getCategories);
+
+  const getCategories = async () => {
+    const res = await fetch(
+      `https://ecommerce-node4.vercel.app/categories/active?page=1&limit=10`
+    );
+    const data = await res.json();
+    console.log(data);
+    setProducts(data.categories);
+  };
+
+  useEffect(() => {
+    console.log(`hello from category`);
+  }, []);
+  return (
+    <>
+      {products.map((prod) => {
+        <h2>{prod.name}</h2>;
+      })}
+    </>
+  );
 }
