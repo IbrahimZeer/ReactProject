ac033cb8e893168f6ec1e8740ce549f5e93ece6d
ibraheem Zeer <211400@ppu.edu.ps>
2024-04-04 22:11:55 +0300

    reg page

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,5 +1,111 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { object, string } from "yup";
+import axios from "axios";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [user, setUser] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    image: "",
+  });
+
+  const [error, setError] = useState([]);
+
+  const handleChanges = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleImageChanges = (e) => {
+    const { name, file } = e.target;
+    setUser({
+      ...user,
+      [name]: file[0],
+    });
+  };
+
+  const validationData = async (e) => {
+    const RegSchema = object({
+      userName: string().min(5).max(20).required(),
+      email: string().email().required(),
+      password: string().min(8).max(20).required(),
+      image: string(),
+    });
+
+    try {
+      await RegSchema.validate(user, { abortEarly: false });
+      return true;
+    } catch (error) {
+      setError(error.errors);
+      return false;
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validate = await validationData();
+    const formData = new FormData();
+    formData.append("userName", user.userName);
+    formData.append("email", user.email);
+    formData.append("password", user.password);
+    formData.append("image", user.image);
+    const { data } = axios.post(
+      `${import.meta.env.VITE_API_URL}/auth/ignup`,
+      formData
+    );
+    setUser({
+      userName: "",
+      email: "",
+      password: "",
+      image: "",
+    });
+  };
+  return (
+    <>
+      <h2>Register Page</h2>
+      {error.length > 0
+        ? error.map((err) => <span key={err.id}>{err}</span>)
+        : ""}
+
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">user name</label>
+        <input
+          type="text"
+          value={user.userName}
+          name="userName"
+          onChange={handleChanges}
+        />
+
+        <label htmlFor="">email</label>
+        <input
+          type="email"
+          value={user.email}
+          name="email"
+          onChange={handleChanges}
+        />
+
+        <label htmlFor="">password</label>
+        <input
+          type="password"
+          value={user.password}
+          name="password"
+          onChange={handleChanges}
+        />
+
+        <label htmlFor="">image</label>
+        <input
+          type="file"
+          value={user.image}
+          name="image"
+          onChange={handleImageChanges}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
